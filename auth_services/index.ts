61943315242f6ac4ecc96737cd4cc8098dd4eac6
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2025-04-29 14:37:48 +0300

    refactor(index.ts): add middleware and route handling for auth service

--- auth_services/index.ts
+++ auth_services/index.ts
@@ -1,13 +1,22 @@
 import express from 'express';
+import cors from 'cors';
 import dotenv from 'dotenv';
+
+import routes from './routes/index';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT_URL_AUTH || 3001;
 
+// Middleware
 app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
 
+// Routes
+
+app.use(routes);
 app.get('/', (req, res) => {
   res.send('Auth Service is running');
 });
